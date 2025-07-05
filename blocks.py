c1230a3fdfd5142ea90ee4743d17a13d6414e511
Jeffrey Ke <jeffke613@gmail.com>
2025-07-05 10:20:45 -0700

    Implemented MHA.

--- blocks.py
+++ blocks.py
@@ -1,8 +1,16 @@
+import math
+
 import torch
 from torch import nn
 from torch.nn import Functional as F
 
 """
+Another thing that's useful to ask: 
+    How much data did the ViT need to get competitive performance 
+    with the best ResNet? Is is true that transformers need more
+    data?
+
+
 What are the best community implementations of
 these modules? Look them up, don't handroll 
 your own. i.e. does xformers do this better, 
@@ -17,12 +25,109 @@ Remember that a failure is more interesting than a
 success here.
 
 """
+class MultiHeadedAttention(nn.Module):
+    def __init__(self, num_heads, d_model, d_k, d_v):
+        super().__init__()
+        ### Modules
+        self.key_proj = nn.Linear(d_model, num_heads * d_k)
+        self.query_proj = nn.Linear(d_model, num_heads * d_k)
+        self.value_proj = nn.Linear(d_model, num_heads * d_v)
 
+        self.output_proj = nn.Linear(num_heads * d_v, d_model)
+
+        ### State variables
+        self.num_heads = num_heads
+        self.d_k = d_k
+        self.d_v = d_v
+
+    def forward(self, keys, queries, values):
+        """
+        Separating keys, queries, values so I can do cross attention if needed.
+        """
+        assert(keys.shape[-1] == self.d_k and values.shape[-1] == self.d_v)
+        batches, num_tokens, *_ = tokens.shape
+        num_heads = self.num_heads
+        d_k = self.d_k
+        d_v = self.d_v
+
+        keys = (
+                self.key_proj(keys)
+                .view(batches, num_tokens, num_heads, d_k)
+                .transpose(1, 2)
+        ) #b,h,s,k
+        queries = (
+                self.query_proj(queries)
+                .view(batches, num_tokens, num_heads, d_k)
+                .transpose(1, 2)
+        )
+        values = (
+                self.value_proj(values)
+                .view(batches, num_tokens, num_heads, d_v)
+                .transpose(1, 2)
+        )
+
+
+        scores = (
+                torch.einsum(
+                    'bhik,bhjk->bhij',
+                    queries,
+                    keys,
+                )
+                / math.sqrt(d_k)
+        )
+        output_toks = (
+                torch.einsum(
+                        'bhis,bhsv->bhiv',
+                        F.softmax(scores, dim=-1),
+                        values,
+                )
+                .tranpose(1,2)
+                .view(batches, num_tokens, num_heads * d_v)
+        )
+
+
+
+    class EncoderBlock(nn.Module):
+        def __init__(self, num_heads, d_model, d_internal, d_ff):
+            super().__init__()
+            ### state variables
+            self.d_k = d_v =  d_internal // num_heads
+            self.d_internal = d_internal
+
+            ### modules
+            self.mha = MultiHeadedAttention(num_heads, d_model, self.d_k, d_v)
+            self.proj = nn.Sequential(
+                    [
+                        nn.Linear(d_internal, d_ff),
+                        nn.GELU(),
+                        nn.Linear(d_ff, d_model),
+                        nn.GELU(),
+                    ]
+            )
+
+
+        def forward(self, tokens):
+            d_k = self.d_k
+            d_internal = self.d_internal
+
+            tokens = F.layer_norm(tokens, d_k) 
+            tokens = (
+                    self.mha(tokens, tokens, tokens)
+                    + tokens
+            )
+            tokens = F.layer_norm(tokens, d_internal)
+            tokens = (
+                    self.proj(tokens)
+                    + tokens
+            )
+            return tokens
 
 class TransformerEncoder(nn.Module):
-    def __init__(self, ):
+    def __init__(self, num_blocks):
         super().__init__()
-
+        self.encoder_blocks = nn.Sequential(
+                [EncoderBlock()] * num_blocks
+        )
 
     # I like this workflow where I do the forward function
     # first and then do __init__: basically, the important
@@ -30,7 +135,7 @@ class TransformerEncoder(nn.Module):
     # I do the bookkeeping of making sure the object 
     # actually has the members at run-time
     def forward(self, tokens):
-
+        return self.encoder_blocks(tokens)
 
 
 
